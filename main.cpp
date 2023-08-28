0e10c0d0918eb66beab9c85b0e11c4b1eba9ac9a
Kyuwoong Lee <kwooler@gmail.com>
2023-08-27 18:39:46 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int rdnum1, rdnum2, rdnum3;
+
+  /* ************************************
+  You should make a code segment to generate three random numbers.
+  And calculate the summation and average of random numbers.
+    Code your program here
+  ************************************ */
+
+  cout << setprecision(2) << fixed;
+  cout << "Random values are " << rdnum1 << "\t" << rdnum2 << "\t" << rdnum3 << endl;
+  cout << "Total summation: " << total << "\t"
+       << "Average "
+       << "\t" << average << endl;
+}
